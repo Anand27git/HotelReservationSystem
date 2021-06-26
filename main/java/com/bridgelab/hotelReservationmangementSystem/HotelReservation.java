656cceb78ac1a9829d73133f24d44580a0ea08c0
Anand27git <anandkumar256k@gmail.com>
2021-06-26 08:52:25 +0530

    welcome to hotel reservation

---
+++ main/java/com/bridgelab/hotelReservationmangementSystem/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelab.hotelReservationmangementSystem;
+
+public class HotelReservation {
+
+	public static void main(String orgs[])
+	{
+		System.out.println("Welcome to Hotel Reservation Management System");
+	}
+}
